a80eaaf4474d38dd333953d441d90eae488af46f
decca_dev <medcherbani@gmail.com>
2020-12-03 17:22:01 +0000

    added work command

---
+++ commands/economy/work.js
@@ -0,0 +1,30 @@
+const discord = require('discord.js');
+const Data = require('../../models/data');
+
+module.exports = {
+    description: 'Work with seasonal winter jobs and get some snowflakes!',
+    cooldown: '2m',
+    category: 'Economy',
+    callback: async (message) => {
+
+        let lines = require('../../job-lines.json');
+
+        let randomLine = lines[Math.floor(Math.random() * lines.length)];
+
+        let income = Math.round(Math.random() * 50) + 300
+
+        const result = await Data.findOneAndUpdate({
+            userId: user.user.id,
+        }, {
+            userId: user.user.id,
+            $inc: {
+                coins: income,
+            }
+        }, {
+            upsert: true,
+            new: true,
+        })
+
+        message.channel.send(randomLine + " " + income + " snowflakes ❄")
+    }
+}
