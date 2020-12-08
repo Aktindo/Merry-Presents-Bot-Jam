011af35fa9521fdb2a0dd9179bcc28a7c4a4182e
Aktindo <71206933+Aktindo@users.noreply.github.com>
2020-12-08 18:59:56 +0530

    added type propert on shop

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -10,32 +10,38 @@ module.exports = {
             "🎄 Christmas Tree": {
                 key: 'christmas-tree',
                 description: 'A home looks incomplete without an xmas tree.',
-                price: 1050
+                price: 1050,
+                Type: 'Collectable',
             },
             "🔔 Xmas Bells": {
                 key: 'bells',
                 description: 'Yooo, the bells sound hectically amazing!',
                 price: 300,
+                Type: 'Collectable',
             },
             "🎁 Gift": {
                 key: 'gift',
                 description: 'Gift someone a random item!',
                 price: 5000,
+                Type: 'Usable',
             },
             "🎅 Santa Hat": {
                 key: 'santa-hat',
                 description: 'Very fluffy ikr',
                 price: 2500,
+                Type: 'Collectable',
             },
             "🧝‍♂️ Mystery Elf": {
                 key: 'mystery-elf',
                 description: 'The mystery elf, here to help you.',
                 price: 10000,
+                Type: 'Collectable',
             },
             "❄ Rare Snowflake": {
                 key: 'rare-snowflake',
                 description: 'Ooooh, formed from the most dense water ever, the _rare snowflake_!',
                 price: 50000,
+                Type: 'Collectable',
             }
         }
         const Discord = require('discord.js')
@@ -44,7 +50,7 @@ module.exports = {
         .setColor('BLUE')
         .setFooter('Heyo, wanna buy something? Use [prefix]buy <item-key> to buy something!')
         for (const [key, value] of Object.entries(firstItems)) {
-            embed.addField(key, `**Item Key:** \`${value.key}\`\n**Description:** ${value.description}\n**Price:** ${value.price}:snowflake:`, true)
+            embed.addField(key, `**Item Key:** \`${value.key}\`\n**Description:** ${value.description}\n**Price:** ${value.price} :snowflake:\n**Type:** ${value.Type}`, true)
         }
         
         message.channel.send(embed)
